62ec4c8f1d2adfdc851214cef1146a92eb13c1e6
Aaron <aaron@Aarons-MacBook-Pro.local>
2017-10-10 14:41:38 +0800

    Removed socket.io

--- app.js
+++ app.js
@@ -1,24 +1,16 @@
-const webpack = require('webpack');
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
-const config = require('./webpack.base');
 const path = require('path');
 const express = require('express');
 const app = express();
-const isDeveloping = process.env.NODE_ENV !== 'production';
-const port = isDeveloping ? 9999 : process.env.PORT;
-const domain = '0.0.0.0';
+app.isDeveloping = process.env.NODE_ENV !== 'production';
 
-export const server = app.listen(port, function onStart(err) {
-  if (err) {
-    console.log(err);
-  }
-  // console.info(server.address());
-  console.info('==> 🌎 Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
-});
-const io = require('socket.io')(server); // create socket.io server
+const helmet = require('helmet');
+app.use(helmet());
 
-if (isDeveloping) {
+if (app.isDeveloping) {
+  const webpack = require('webpack');
+  const webpackMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const config = require('./webpack.base');
   const compiler = webpack(config);
   const middleware = webpackMiddleware(compiler, {
     publicPath: config.output.publicPath,
@@ -40,13 +32,15 @@ if (isDeveloping) {
     res.end();
   });
 } else {
-  app.use(express.static(__dirname + '/dist'));
+  // app.use(express.static(__dirname + '/dist'));
+  app.disable('x-powered-by');
   console.log('Using production build');
 }
 
-io.on('connection', function(socket) {
-	socket.on('send coordinates', function(data) {
-		socket.emit('hello');
-	});
-});
+const port = app.isDeveloping ? 8080 : process.env.PORT;
 
+app.listen(port, () => {
+  console.log("Starting server at port ", port);
+})
+
+export default app;
